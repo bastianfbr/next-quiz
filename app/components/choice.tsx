64c8463b6db13df04532a5dabc561caeea7d11b6
bastianfbr <badmaxyt@gmail.com>
2023-12-04 23:06:12 +0100

    feat: :sparkles: add rollout of questions and answers

--- app/components/choice.tsx
+++ app/components/choice.tsx
@@ -1,23 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface ChoiceProps {
   color: keyof typeof colorMap;
   content: string;
+  isCorrect: boolean;
+  onClick: () => void;
 }
 
 const colorMap = {
-  "blue": "btn-info",
-  "green": "btn-success",
-  "orange": "btn-warning",
-  "red": "btn-error",
-}
+  blue: 'btn-info',
+  green: 'btn btn-primary',
+  orange: 'btn-warning',
+  magenta: 'btn btn-secondary',
+};
 
-const Choice: React.FC<ChoiceProps> = ({ color, content }) => {
+const Choice: React.FC<ChoiceProps> = ({ color, content, isCorrect, onClick }) => {
+  const [showDialog, setShowDialog] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowDialog(true);
+
+    setTimeout(() => {
+      setShowDialog(false);
+    }, 500);
+    
+    onClick();
+  };
+
   const buttonClassName = `btn btn-lg ${colorMap[color]}`;
+  const alertClassName = isCorrect ? 'alert alert-success' : 'alert alert-error';
 
   return (
-    <div className="join join-vertical">
-      <button className={buttonClassName}>{content}</button>
+    <div className="join join-vertical mb-5">
+      <button className={buttonClassName} onClick={handleButtonClick}>
+        {content}
+      </button>
+
+      {showDialog && (
+        <div className="toast toast-center">
+          <div className={alertClassName}>
+            <span>{isCorrect ? "C'est la bonne réponse !" : "Ce n'est pas la bonne réponse."}</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,42 @@
+'use client'
+
+import React, { useState } from 'react';
 import Choice from './components/choice';
 import Question from './components/question';
 
+const questions = [
+  "Voici la première question 1",
+  "Nouvelle question 2",
+  "Encore une nouvelle question 3",
+  "La dernière question 4",
+];
+
+const answers = [1, 2, 3, 4];
+
 export default function Home() {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+  const handleAnswerClick = (selectedAnswer: number) => {
+    const isCorrect = selectedAnswer === answers[currentQuestionIndex];
+    console.log(`User selected answer ${selectedAnswer} for question ${currentQuestionIndex + 1}`);
+
+    // You can use the result of the check for further logic or storage
+    console.log(`User's answer is correct: ${isCorrect}`);
+
+    setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+
+    if (currentQuestionIndex === questions.length - 1) {
+      setCurrentQuestionIndex(0);
+    }
+  };
+
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-white">
-      <Question question="Quelle est la réponse à la vie, l'univers et le reste ?" />
-      <Choice color="red" content="Ceci est une réponse 1" />
-      <Choice color="green" content="Peut-être la réponse 2" />
-      <Choice color="orange" content="Ou alors la réponse 3" />
-      <Choice color="blue" content="Finalement réponse 4 ?" />
+      <Question question={questions[currentQuestionIndex]} />
+      <Choice color="magenta" content="Ceci est une réponse 1" isCorrect={answers[currentQuestionIndex] === 1} onClick={() => handleAnswerClick(1)} />
+      <Choice color="green" content="Peut-être la réponse 2" isCorrect={answers[currentQuestionIndex] === 2} onClick={() => handleAnswerClick(2)} />
+      <Choice color="orange" content="Ou alors la réponse 3" isCorrect={answers[currentQuestionIndex] === 3} onClick={() => handleAnswerClick(3)} />
+      <Choice color="blue" content="Finalement réponse 4 ?" isCorrect={answers[currentQuestionIndex] === 4} onClick={() => handleAnswerClick(4)} />
     </main>
   );
 }
